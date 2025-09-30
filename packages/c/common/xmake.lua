32080b90e5ab2c140eb20d4730445c5334fec367
mono-cyte <a578448983@gmail.com>
2025-10-01 00:24:49 +0800

    check plat

--- packages/c/common/xmake.lua
+++ packages/c/common/xmake.lua
@@ -12,7 +12,7 @@ package("common")
     add_versions("0.0.1", "46a5ad14e7d2b1b65f4bdc8e7c9b079f7728b2eb5932b71433c6ac76a630f517")
 
 
-    on_install(function (package)
+    on_install("windows|x64", function (package)
         local configs = {}
         import("package.tools.xmake").install(package, configs)
     end)
