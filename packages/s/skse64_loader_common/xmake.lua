24e08c906438097565062d8cbc6fbc2df21fc521
mono-cyte <a578448983@gmail.com>
2025-10-02 20:06:59 +0800

    fix

--- packages/s/skse64_loader_common/xmake.lua
+++ packages/s/skse64_loader_common/xmake.lua
@@ -9,16 +9,8 @@ package("skse64_loader_common")
 
     on_install(function (package)
         local configs = {}
-        io.writefile("xmake.lua", [[
-            add_rules("mode.release", "mode.debug")
-            target("skse64_loader_common")
-                set_kind("")
-                add_files("src/*.c")
-                add_headerfiles("src/(*.h)")
-        ]])
         import("package.tools.xmake").install(package)
     end)
 
     on_test(function (package)
-        assert(package:has_cfuncs("foo", {includes = "foo.h"}))
     end)
